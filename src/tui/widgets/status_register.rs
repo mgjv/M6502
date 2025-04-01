f8560bcf670a98858e6c2564f62186b48f47dcdb
Martien Verbruggen <martien.verbruggen@gmail.com>
2025-04-01 16:30:54 +0200

    further simplified status register widget.

--- src/tui/widgets/status_register.rs
+++ src/tui/widgets/status_register.rs
@@ -3,6 +3,7 @@ use crate::computer::cpu;
 use ratatui::buffer::Buffer;
 use ratatui::style::{Color, Style};
 use ratatui::layout::Rect;
+use ratatui::text::{Line, Span};
 use ratatui::widgets::Widget;
 
 #[derive(Clone, Copy)]
@@ -18,45 +19,31 @@ impl StatusRegisterWidget {
     }
 }
 
-#[derive(Clone, Copy)]
-struct StatusBitWidget {
-    value: bool,
-    name: char,
-}
-
 // This widget displays the value of a status register.
-// If the space is narrower than 8, it will simpluy display a
-// hex value, under a header SR. If larger than 8, it will display
-// 8 bit statuses next to each other.
 impl Widget for StatusRegisterWidget {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let bits = [
-            StatusBitWidget { value: self.status.negative, name: 'N' },
-            StatusBitWidget { value: self.status.overflow, name: 'V' },
-            StatusBitWidget { value: self.status.ignored,  name: '-' },
-            StatusBitWidget { value: self.status.brk, name: 'B' },
-            StatusBitWidget { value: self.status.decimal, name: 'D' },
-            StatusBitWidget { value: self.status.irq_disable, name: 'I' },
-            StatusBitWidget { value: self.status.zero, name: 'Z' },
-            StatusBitWidget { value: self.status.carry, name: 'C' },
-        ];
-        for (i, bit) in bits.iter().enumerate() {
-            let area = Rect::new(
-                area.x + (i as u16),
-                area.y,
-                1,
-                area.height,
-            );
-            bit.render(area, buf);
-        }
+        let mut spans = vec![];
 
+        spans.push(Span::raw("SR: "));
+
+        spans.push(self.bit_span('N', self.status.negative));
+        spans.push(self.bit_span('V', self.status.overflow));
+        spans.push(self.bit_span('-', self.status.ignored));
+        spans.push(self.bit_span('B', self.status.brk));
+        spans.push(self.bit_span('D', self.status.decimal));
+        spans.push(self.bit_span('I', self.status.irq_disable));
+        spans.push(self.bit_span('Z', self.status.zero));
+        spans.push(self.bit_span('C', self.status.carry));
+
+        let line = Line::from(spans);
+        line.render(area, buf);
     }
 }
 
-impl Widget for StatusBitWidget {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let style = Style::default().fg(if self.value { Color::LightYellow } else { Color::DarkGray });
-        let name = if self.value { self.name.to_string() } else { self.name.to_lowercase().to_string() };
-        buf.set_string(area.x, area.y, name, style);
+impl StatusRegisterWidget {
+    fn bit_span(&self, name: char, status: bool) -> Span {
+        let style = Style::default().fg(if status { Color::LightYellow } else { Color::DarkGray });
+        let name = if status { name.to_string() } else { name.to_lowercase().to_string() };
+        Span::styled(name, style)
     }
 }
